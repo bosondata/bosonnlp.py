d02207eea625711e630064be6b228fb63bf81935
Ashin Chen <ashin.chen@bosondata.com.cn>
2014-12-17 15:38:19 +0800

    Bump version: 0.4.0 → 0.5.0

--- bosonnlp/__init__.py
+++ bosonnlp/__init__.py
@@ -33,7 +33,7 @@ BosonNLP HTTP API 封装库（SDK）。
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 
-__VERSION__ = '0.4.0'
+__VERSION__ = '0.5.0'
 
 
 from .client import BosonNLP, ClusterTask, CommentsTask
